a9d38c09aaf8822505842e9cd24727db149ceaad
Neha Patil <nehapatil@Nehas-MacBook-Pro.local>
2026-05-20 09:51:12 -0700

    Day 1- variables, data types, functions

---
+++ basics.js
@@ -0,0 +1,26 @@
+// My first Javascript practice 
+let myName ="Neha";
+let myGoal="Frontend Developer in Bay Area";
+let myCity="Bay Area";
+let myExperience=5;
+let isLookingForJob= true;
+let yearsOfBreak= 5;
+let isReady= true;
+
+console.log("My name is " + myName);
+console.log("My goal is " + myGoal); 
+console.log("I live in " + myCity);
+console.log("I have " + myExperience + " years of experience");
+console.log("Looking for Job: " + isLookingForJob);
+console.log("Years of break: " + yearsOfBreak);
+console.log("Ready to work: " + isReady);
+console.log("Let's build something great!");
+
+function greetDeveloper(name){
+    console.log("Welcome back to coding, " + name + " ! ");
+    console.log("You are going to do great things");
+}
+
+greetDeveloper("Neha");
+greetDeveloper("Aashvi");
+greetDeveloper("Future Developer");
